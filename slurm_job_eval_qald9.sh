0a915237ce7e7e9ca3633aac0bd32b8e63344493
Corsin Capol <capol@uni-hildesheim.de>
2024-09-16 15:57:48 +0200

    slurm eval job

---
+++ slurm_job_eval_qald9.sh
@@ -0,0 +1,11 @@
+#!/bin/bash -l
+#SBATCH --job-name="SGPT-SPARQL-query-generation: EVAL QALD-9"
+#SBATCH --time=06:00:00
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-core=1
+#SBATCH --ntasks-per-node=12
+#SBATCH --cpus-per-task=1
+#SBATCH --partition=staff
+#SBATCH --gpus-per-node=a100:1
+
+srun python -u eval.py --generate runs/sgpt/qald9/ --dataset qald9 --generation_params_file config/gpt-2-base/generation_params.json --eval_dataset test  --output_file outputs/predictions_gpt2-base.json 
