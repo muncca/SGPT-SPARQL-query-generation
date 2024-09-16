40c7c67e4396f9a151a79ce70f00b3501e04ee3f
Corsin Capol <capol@uni-hildesheim.de>
2024-09-16 14:30:50 +0200

    requirement versions added. slurm file added for job scheduling.

---
+++ slurm_job_train_qald9.sh
@@ -0,0 +1,12 @@
+#!/bin/bash -l
+#SBATCH --job-name="SGPT: TRAIN QALD-9"
+#SBATCH --time=15:00:00
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-core=1
+#SBATCH --ntasks-per-node=64
+#SBATCH --cpus-per-task=1
+#SBATCH --partition=staff
+#SBATCH --gpus-per-node=a100:1
+#SBATCH --partition=staff
+
+srun python ~/dev/SGPT-SPARQL-query-generation/train.py --dataset qald9 --epochs 70
